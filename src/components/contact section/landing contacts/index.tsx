2fe3f44e9815143175ce7442ab8b36cfe541ef9b
RFLX36 <rolandfonzlamoste3608@gmail.com>
2026-04-14 01:24:39 +0800

    temporary deployment fix

--- src/components/contact section/landing contacts/index.tsx
+++ src/components/contact section/landing contacts/index.tsx
@@ -17,6 +17,7 @@ export default function LandingContacts() {
 
     useEffect(() => {
         fetchGithubData();
+        console.log(githubData);
     }, []);
 
     const cursorOnHoverCV = useCursor({ tooltip: 'Download Resume', type: "pointer" })
